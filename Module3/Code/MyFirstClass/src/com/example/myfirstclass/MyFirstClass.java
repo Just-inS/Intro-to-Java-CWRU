b4704b46765c809b4e1ce141bffba55d3c1d8624
Justin Sparto <spartojustin@gmail.com>
2022-12-12 17:18:31 -0500

    Begin Module 3

---
+++ Module3/Code/MyFirstClass/src/com/example/myfirstclass/MyFirstClass.java
@@ -0,0 +1,8 @@
+package com.example.myfirstclass;
+
+/* This is my first class in Java */
+public class MyFirstClass extends Object{
+    public static void main(String[] args) {
+        System.out.print("Hello, World \n");
+    }
+}
